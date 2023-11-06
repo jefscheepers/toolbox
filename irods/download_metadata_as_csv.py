1b25195c2a83f04e03becf02e68f20d45040781b
Jef Scheepers <jef.scheepers@kuleuven.be>
2023-11-06 14:08:37 +0100

    feat: add script to download metadata

---
+++ irods/download_metadata_as_csv.py
@@ -0,0 +1,37 @@
+"""Write metadata on an object to CSV"""
+
+import os
+from pathlib import Path
+import ssl
+import csv
+from argparse import ArgumentParser
+from irods.session import iRODSSession
+
+
+def main(session, path):
+    obj = session.data_objects.get(path)
+    do_name = obj.name
+    print(f"Downloading metadata for {do_name}")
+    metadata = obj.metadata.items()
+
+    do_without_extension = Path(path).stem
+    csv_filename = f"{do_without_extension}_metadata.csv"
+
+    with open(csv_filename, "w") as file:
+        file.write("Attribute,value,units\n")
+        for avu in metadata:
+            file.write(f"{avu.name},{avu.value},{avu.units}\n")
+
+
+if __name__ == "__main__":
+    # Handling commandline arguments
+    parser = ArgumentParser(usage=__doc__)
+    parser.add_argument(dest="path", help="The path to the data object")
+    args = parser.parse_args()
+
+    try:
+        env_file = os.environ["IRODS_ENVIRONMENT_FILE"]
+    except KeyError:
+        env_file = os.path.expanduser("~/.irods/irods_environment_prc.json")
+    with iRODSSession(irods_env_file=env_file) as session:
+        main(session, args.path)

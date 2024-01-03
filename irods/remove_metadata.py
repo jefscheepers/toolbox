e14582444d6c7645ceccd15bdf4cfb47371fdba7
Jef Scheepers <jef.scheepers@kuleuven.be>
2024-01-03 10:50:57 +0100

    feat: add recursive option

--- irods/remove_metadata.py
+++ irods/remove_metadata.py
@@ -7,30 +7,53 @@ from irods.meta import iRODSMeta, AVUOperation
 from irods.session import iRODSSession
 
 
-def remove_all_avus(Object):
+def remove_all_avus(Object, verbose=False):
+    print(f"Removing metadata from {Object.path}")
     # taken from examples at https://github.com/irods/python-irodsclient
-
     avus_on_Object = Object.metadata.items()
-    Object.metadata.apply_atomic_operations( *[AVUOperation(operation='remove', avu=i) for i in avus_on_Object] )
+    Object.metadata.apply_atomic_operations(
+        *[AVUOperation(operation="remove", avu=i) for i in avus_on_Object]
+    )
 
 
-def main(session, path):
-    
+def main(session, path, recursive=False):
+    # if given path is a collection
     try:
         coll = session.collections.get(path)
         remove_all_avus(coll)
+        if recursive:
+            data_objects = coll.data_objects
+            for obj in data_objects:
+                remove_all_avus(obj)
+            subcollections = coll.subcollections
+            for subcollection in subcollections:
+                main(session, subcollection.path, recursive=True)
     except CollectionDoesNotExist:
-        try: 
+        # if given path is a data object
+        try:
             obj = session.data_objects.get(path)
+            if recursive:
+                raise Exception(
+                    f"You cannot use a recursive operation on a data object."
+                )
             remove_all_avus(obj)
         except DataObjectDoesNotExist:
-            raise Exception(f"{path} is not recognized as object or collection in iRODS")
+            # if given path doesn't exist
+            raise Exception(
+                f"{path} is not recognized as object or collection in iRODS"
+            )
 
-        
+
 if __name__ == "__main__":
     # Handling command-line arguments
     parser = ArgumentParser(usage=__doc__)
     parser.add_argument(dest="path", help="The path of the data object or collection")
+    parser.add_argument(
+        "-r",
+        dest="recursive",
+        action="store_true",
+        help="Remove metadata from a collection and all its contents, including subcollections",
+    )
     args = parser.parse_args()
 
     # creating iRODS session
@@ -44,4 +67,4 @@ if __name__ == "__main__":
     )
     ssl_settings = {"ssl_context": ssl_context}
     with iRODSSession(irods_env_file=env_file, **ssl_settings) as session:
-        main(session, args.path)
+        main(session, args.path, args.recursive)
